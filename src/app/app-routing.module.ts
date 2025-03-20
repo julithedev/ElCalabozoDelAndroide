567f34ba35a117bb26b80b83f3b1c9ba95150b67
julithedev <juliancito8790@gmail.com>
2025-03-20 18:06:38 -0500

    Cambiando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './components/productos/productos.component';
 
-const routes: Routes = [
-  { path: '', component: ProductosComponent },
-  // { path: 'productos/:nombre', component: ProductoDetalleComponent },
-];
+const routes: Routes = [{ path: '', component: ProductosComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
